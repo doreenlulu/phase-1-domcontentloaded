5ebc3dc49dcacc9dd7a3ac58d9a8971fb55d456b
Doreen <doreenlulu97@gmail.com>
2022-08-28 02:30:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+  });
+
+  document.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementById('text').textContent = "This is really cool!"
+}); 
